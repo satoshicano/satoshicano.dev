8d8c9f0bf48705feb41c7a48597830c315f6b057
satoshicano <satoshicano@gmail.com>
2024-06-09 18:24:45 +0900

    configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,13 @@
-module.exports = {
-	env: {
-		node: true,
-		browser: true,
-		es2022: true,
-	},
-	overrides: [
-		{
-			// Define the configuration for `.astro` file.
-			files: ["*.astro"],
-			// Allows Astro components to be parsed.
-			parser: "astro-eslint-parser",
-			// Parse the script in `.astro` as TypeScript by adding the following configuration.
-			// It's the setting you need when using TypeScript.
-			parserOptions: {
-				parser: "@typescript-eslint/parser",
-				extraFileExtensions: [".astro"],
-			},
-			rules: {
-				// override/add rules settings here, such as:
-				// "astro/no-set-html-directive": "error"
-			},
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+	// add more generic rule sets here, such as:
+	// js.configs.recommended,
+	...eslintPluginAstro.configs.recommended,
+	{
+		rules: {
+			// override/add rules settings here, such as:
+			// "astro/no-set-html-directive": "error"
 		},
-	],
-	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:astro/recommended",
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
 	},
-	plugins: ["@typescript-eslint"],
-	rules: {},
-};
+];
