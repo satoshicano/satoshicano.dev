c79c0b0b94aa2437828523dce3b990b8788f2402
satoshicano <satoshicano@gmail.com>
2025-02-10 00:50:52 +0900

    remove all

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
-export default defineConfig({
-	site: "https://satoshicano.dev/",
-	integrations: [mdx(), sitemap()],
-});
