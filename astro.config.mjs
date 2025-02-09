985eb4af11b6c6a43027c0418612a4a42a092d16
satoshicano <satoshicano@gmail.com>
2025-02-10 01:06:18 +0900

    rename: update site URL to "https://satoshicano.dev" in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: "https://satoshicano.dev",
 	integrations: [mdx(), sitemap()],
 });
